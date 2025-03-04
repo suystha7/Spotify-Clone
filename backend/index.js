723530cd7a2659af9d4e7af1a0d7bc6921419f01
suyogstha7 <suyogstha317@gmail.com>
2025-03-04 22:30:33 +0545

    initiate

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+
+import userRoutes from "./routes/user.route.js"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(morgan("dev"));
+
+app.get("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
